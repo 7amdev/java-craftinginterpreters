c89cfac549cbead71cc7f6dc54b4c9e4409d9f06
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-03-26 06:42:09 -0100

    add prompt; load code from a file'

--- code/F7.java
+++ code/F7.java
@@ -1,13 +1,18 @@
 package code;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class F7 {
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
+            System.out.println("Usage: jf7 [script]");
         } else if (args.length == 1) {
-            runFile("");
+            runFile(args[0]);
         } else {
             runPrompt();
         }
@@ -15,11 +20,27 @@ public class F7 {
     }
 
     private static void runFile(String path) throws IOException {
-        System.out.println("Load file");
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String sourceCode = new String(bytes, Charset.defaultCharset());
+        run(sourceCode);
     }
 
-    private static void runPrompt() {
-        System.out.println(">");
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        System.out.println("Source: " + source);
     }
 
 }
